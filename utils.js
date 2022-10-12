8cd32928a71fa47aa9e879ff742d0060fb48a0b6
Tahmid406 <39864760+Tahmid406@users.noreply.github.com>
2022-10-12 23:09:02 +0600

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,3 +1,14 @@
 const sensorDetect = (sensorRadius, position1, position2) => {
-  return pow(position1.y - position2.y, 2) + pow(position1.x - position2.x, 2) <= pow(sensorRadius, 2);
+  return (
+    pow(position1.y - position2.y, 2) + pow(position1.x - position2.x, 2) <= pow(sensorRadius, 2)
+  );
 };
+
+class Obstacle {
+  constructor() {
+    this.position = createVector(random(width), random(height));
+  }
+  render() {
+    circle(this.position.x, this.position.y, 8);
+  }
+}
