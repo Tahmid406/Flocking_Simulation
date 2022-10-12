037542f1fdd7912290f8b7a972e1ac801b712443
Tahmid406 <39864760+Tahmid406@users.noreply.github.com>
2022-10-12 23:08:29 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 let boids = [];
+let obstacles = [];
 
 function setup() {
   createCanvas(innerWidth, innerHeight);
-  fill(255);
-  stroke(255);
+  noStroke();
 
-  for (let i = 0; i < 200; i++) boids.push(new Boid());
+  for (let i = 0; i < 100; i++) boids.push(new Boid());
+  for (let i = 0; i < 10; i++) obstacles.push(new Obstacle());
 }
 
 function draw() {
-  background(0);
+  background(25, 36, 47);
 
   for (let boid of boids) {
     boid.update();
     boid.render();
   }
+  fill(255, 0, 0);
+  for (let obstacle of obstacles) obstacle.render();
 }
